534d9c210d55e94f95cd99d6bda3b989d61ab148
Xitong Gao <gxtfmx@gmail.com>
2017-08-17 16:45:56 +0800

    Faster CLI

--- mayo/__init__.py
+++ mayo/__init__.py
@@ -1,24 +1,11 @@
 import os
-import sys
-
-__mayo__ = 'Mayo'
-__description__ = """
-A deep learning framework with condiment preference as religion."""
-__version__ = '0'
-__date__ = '14 Aug 2017'
-__author__ = 'Xitong Gao'
-__executable__ = os.path.basename(sys.argv[0])
-__doc__ = """
-{__mayo__} {__version__} {__date__}
-{__description__}
-{__author__}
-""".format(**locals())
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
-
 
 from mayo.config import Config
 from mayo.net import Net
 from mayo.train import Train
+from mayo.cli import meta
 
 __all__ = [Config, Net, Train]
+locals().update(meta())
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
--- mayo/cli.py
+++ mayo/cli.py
@@ -1,30 +1,44 @@
 import os
+import sys
 
+import yaml
 from docopt import docopt
 
-import mayo
-from mayo.config import Config
-from mayo.train import Train
-from mayo.evaluate import Evaluate
+__root__ = os.path.dirname(__file__)
 
-
-usage = """
-{__mayo__} {__version__} {__date__}
+_DOC = """
+{__mayo__} {__version__} ({__date__})
 {__description__}
 {__author__}
-
+"""
+_USAGE = """
 Usage:
     {__executable__} train <net-yaml> <dataset-yaml> [--train=<train-yaml>]
     {__executable__} (-h | --help)
-""".format(**vars(mayo))
+"""
 
 
-_mayo_root = os.path.dirname(mayo.__file__)
-DEFAULT_TRAIN_YAML = os.path.join(_mayo_root, 'train.yaml')
+def meta():
+    meta_file = os.path.join(__root__, 'meta.yaml')
+    meta_dict = yaml.load(open(meta_file, 'r'))
+    meta_dict['__executable__'] = os.path.basename(sys.argv[0])
+    return meta_dict
+
+
+def doc():
+    return _DOC.format(**meta())
+
+
+def usage():
+    return doc() + _USAGE.format(**meta())
 
 
 def train(args):
-    train_yaml = args['--train'] or DEFAULT_TRAIN_YAML
+    from mayo.config import Config
+    from mayo.train import Train
+
+    default_train_yaml = os.path.join(__root__, 'train.yaml')
+    train_yaml = args['--train'] or default_train_yaml
     config = Config(
         net=args['<net-yaml>'], dataset=args['<dataset-yaml>'],
         train=train_yaml)
@@ -32,12 +46,15 @@ def train(args):
 
 
 def validate(args):
+    from mayo.config import Config
+    from mayo.evaluate import Evaluate
+
     config = Config(net=args['<net-yaml>'], dataset=args['<dataset-yaml>'])
     return Evaluate(config).evaluate()
 
 
 def main():
-    args = docopt(usage, version=mayo.__version__)
+    args = docopt(usage(), version=meta()['__version__'])
     commands = {
         'train': train,
         'validate': validate,
