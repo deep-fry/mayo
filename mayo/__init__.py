eb3293e204e368834c809a317101d514e96495dc
Xitong Gao <gxtfmx@gmail.com>
2017-08-23 18:53:39 +0800

    Minor refactor for eval

--- mayo/__init__.py
+++ mayo/__init__.py
@@ -3,6 +3,7 @@ import os
 from mayo.config import Config
 from mayo.net import Net
 from mayo.train import Train
+from mayo.eval import Evaluate
 from mayo.cli import meta
 
 __all__ = [Config, Net, Train]
