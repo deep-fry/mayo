1bd59c17ee3bfc25b5d9dafc783374790cbd0062
Xitong Gao <gxtfmx@gmail.com>
2017-09-19 11:16:41 +0800

    Fix incorrect num_classes used in cli_info

--- mayo/cli.py
+++ mayo/cli.py
@@ -168,7 +168,7 @@ Arguments:
         config = self.config
         batch_size = config.get('system.batch_size', None)
         images_shape = (batch_size, ) + config.image_shape()
-        labels_shape = (batch_size, config.dataset.num_classes)
+        labels_shape = (batch_size, config.num_classes())
         with tf.Graph().as_default():
             images = tf.placeholder(tf.float32, images_shape, 'images')
             labels = tf.placeholder(tf.int32, labels_shape, 'labels')
