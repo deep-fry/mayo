22be8b2268bdee1ef6b6b189b2a63f63f6377a56
Xitong Gao <gxtfmx@gmail.com>
2017-11-07 23:17:41 +0800

    Remove ipdb.set_trace() from Train._gradients()

--- mayo/session/train.py
+++ mayo/session/train.py
@@ -68,7 +68,6 @@ class Train(Session):
             regularization = tf.get_collection(
                 tf.GraphKeys.REGULARIZATION_LOSSES)
             loss = tf.add_n([net.loss()] + regularization)
-            __import__('ipdb').set_trace()
             return self.optimizer.compute_gradients(loss)
         tower_grads = self.net_map(gradient)
         return self._average_gradients(tower_grads)
