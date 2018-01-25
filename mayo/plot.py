a3a2c11953d575c92e453327a044cddc86f5c745
Xitong Gao <gxtfmx@gmail.com>
2018-01-25 15:55:14 +0800

    Fix various issues introduced in revamped plot

--- mayo/plot.py
+++ mayo/plot.py
@@ -19,10 +19,6 @@ class Plot(object):
         self.net = session.nets[0]
 
     @property
-    def image_format(self):
-        return self.config.system.plot.format
-
-    @property
     def _path(self):
         return self.config.system.search_path.plot[0]
 
@@ -34,19 +30,20 @@ class Plot(object):
         input_image, label, layers, variables = self.session.run(
             [input_tensor, label_tensor, layer_tensors, variable_tensors])
         try:
-            num = self.config.system.batch_size_per_gpu
-            for i in range(num):
-                log.info(
-                    '{}% Plotting image #{}...'
-                    .format(int(i / num * 100.0), i), update=True)
-                path = os.path.join(self._path, str(i))
-                os.makedirs(path, exist_ok=True)
-                # input image
-                # root/{index}/input.{ext}
-                input_path = os.path.join(path, 'input-{}'.format(label[i]))
-                self._plot_rgb_image(input_image[i], input_path)
-                # layer activations
-                if self.config.system.plot.features:
+            if self.config.system.plot.features:
+                num = self.config.system.batch_size_per_gpu
+                for i in range(num):
+                    log.info(
+                        '{}% Plotting image #{}...'
+                        .format(int(i / num * 100.0), i), update=True)
+                    path = os.path.join(self._path, str(i))
+                    os.makedirs(path, exist_ok=True)
+                    # input image
+                    # {root}/{index}/input.{ext}
+                    input_path = os.path.join(
+                        path, 'input-{}'.format(label[i]))
+                    self._plot_rgb_image(input_image[i], input_path)
+                    # layer activations
                     for node, value in layers.items():
                         if value.ndim != 4:
                             # value is not a (N x H x W x C) layout
@@ -55,15 +52,19 @@ class Plot(object):
                         # root/{index}/{layer_name}.{ext}
                         layer_path = os.path.join(path, name)
                         self._plot_images(value[i], layer_path)
-                # overridden variable histogram
-                if self.config.system.plot.parameters:
-                    for node, name_value_map in variables.items():
-                        for name, value in name_value_map.items():
-                            name = '{}-{}'.format(node.formatted_name(), name)
-                            name = name.replace('/', '-')
-                            # root/{index}/{layer_name}/{variable_name}.{ext}
-                            var_path = os.path.join(path, name)
-                            self._plot_histogram(value[i], var_path)
+            # overridden variable histogram
+            if self.config.system.plot.parameters:
+                for node, name_value_map in variables.items():
+                    for name, value in name_value_map.items():
+                        layer_name = node.formatted_name()
+                        log.info(
+                            'Plotting parameter {} in layer {}'
+                            .format(name, layer_name))
+                        name = '{}-{}'.format(layer_name, name)
+                        name = name.replace('/', '-')
+                        # {root}/{layer_name}-{variable_name}.{ext}
+                        var_path = os.path.join(self._path, name)
+                        self._plot_histogram(value, var_path)
         except KeyboardInterrupt:
             log.info('Abort.')
 
@@ -74,8 +75,10 @@ class Plot(object):
                 value = value[:, :, 0]
             elif value.shape[-1] == 3:
                 cmap = None
-        path = '{}.{}'.format(path, self.image_format)
+        path = '{}.{}'.format(path, 'png')
         log.debug('Saving RGB image at {}...'.format(path))
+        min_value = np.min(value)
+        value = (value - min_value) / (np.max(value) - min_value)
         self.pyplot.imsave(path, value, cmap=cmap)
 
     def _plot_images(self, value, path):
@@ -96,7 +99,7 @@ class Plot(object):
             fmap = value[:, :, i]
             ax.imshow(fmap * 255.0 / max_value, cmap='gray')
         fig.subplots_adjust(wspace=0.025, hspace=0.005)
-        path = '{}.{}'.format(path, self.image_format)
+        path = '{}.{}'.format(path, 'png')
         log.debug('Saving grid of images at {}...'.format(path))
         fig.savefig(path)
         self.pyplot.close(fig)
@@ -105,7 +108,7 @@ class Plot(object):
         fig = self.pyplot.figure()
         # histogram
         n, bins, patches = self.pyplot.hist(value.flatten(), bins='fd')
-        path = '{}.{}'.format(path, self.image_format)
+        path = '{}.{}'.format(path, 'eps')
         log.debug('Saving histogram at {}...'.format(path))
         fig.savefig(path)
         self.pyplot.close(fig)
