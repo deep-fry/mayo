788dcfd0c3eb4b86919c5460599118d5a0cba247
Xitong Gao <gxtfmx@gmail.com>
2018-01-29 15:50:15 +0800

    Add back "slow" variant of hadamard

--- mayo/net/tf/hadamard.py
+++ mayo/net/tf/hadamard.py
@@ -1,5 +1,6 @@
 import math
 
+import scipy
 import numpy as np
 import tensorflow as tf
 
@@ -16,17 +17,12 @@ class HadamardLayers(object):
         return tensor * np.reshape(scales, [1, 1, 1, channels])
 
     def instantiate_hadamard(self, node, tensor, params):
-        def fwht(value):
-            if value.shape[-1] == 1:
-                return value
-            lower, upper = tf.split(value, 2, axis=-1)
-            lower, upper = lower + upper, lower - upper
-            return tf.concat((fwht(lower), fwht(upper)), axis=-1)
         # check depth is 2^n
         channels = int(tensor.shape[-1])
         if 2 ** int(math.log(channels, 2)) != channels:
             raise ValueError(
                 'Number of channels must be a power of 2 for hadamard layer.')
+
         # scale channels
         if params.get('variable_scales', False):
             # ensures correct broadcasting behaviour
@@ -36,7 +32,24 @@ class HadamardLayers(object):
             channel_scales = tf.get_variable(
                 name='channel_scale', shape=shape, initializer=init)
             tensor *= channel_scales
-        # hadamard transform
+
+        if params.pop('block', False):
+            hadamard = scipy.linalg.hadamard(channels)
+            hadamard = tf.constant(hadamard, dtype=tf.float32)
+            # flatten input tensor
+            flattened = tf.reshape(tensor, [-1, channels])
+            # transform with hadamard
+            transformed = flattened @ hadamard
+            return tf.reshape(transformed, shape=tensor.shape)
+
+        def fwht(value):
+            if value.shape[-1] == 1:
+                return value
+            lower, upper = tf.split(value, 2, axis=-1)
+            lower, upper = lower + upper, lower - upper
+            return tf.concat((fwht(lower), fwht(upper)), axis=-1)
+
+        # fast walsh-hadamard transform
         tensor = fwht(tensor)
         # activation
         activation_function = params.get('activation_fn', tf.nn.relu)
