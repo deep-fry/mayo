934a84d931aa898377149ee293fc26a96e778464
Xitong Gao <gxtfmx@gmail.com>
2018-03-27 11:39:51 +0800

    Fix MoE regularizer

--- mayo/net/tf/gate/sparse.py
+++ mayo/net/tf/gate/sparse.py
@@ -9,19 +9,27 @@ class SparseRegularizedGatedConvolutionBase(GatedConvolutionBase):
         defaults['epsilon'] = 0.001
 
     def _mixture(self, tensor, axes):
-        mean, variance = tf.nn.moments(tensor, axes=axes)
+        tensor = tf.reduce_sum(tensor, axes)
+        if tensor.shape.ndims != 1:
+            raise ValueError('Tensor should reduce to a vector.')
+        mean, variance = tf.nn.moments(tensor, axes=0)
         return variance / tf.square((mean + self.epsilon))
 
     def regularize(self):
         """
         We use a L1, L2 or MoE regularizer to encourage sparsity in gate.
         """
-        sparse = self.gate() * self.actives()
         func_map = {
             'l1': lambda v: tf.abs(v),
             'l2': lambda v: tf.square(v),
             'moe': lambda v: self._mixture(v, [0, 1, 2]),
             'moi': lambda v: self._mixture(v, [1, 2, 3]),
         }
+        # set inactive elements to zeros
+        # Xitong: it would be possible to use self.actives() to mask out
+        # elements to regularize, but writing this in tensorflow has
+        # proven to be tricky.
+        sparse = self.gate() * self.actives()
+        # add regularization for each specified keys
         for key, weight in self.regularizer.items():
             self._add_regularization(func_map[key](sparse), weight)
