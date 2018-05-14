a1bc10ac3126f0636e6995505d805dcab0f25edb
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2018-05-14 13:30:20 +0100

    refactor and finished retrain simple for layerwise

--- mayo/session/__init__.py
+++ mayo/session/__init__.py
@@ -1,7 +1,8 @@
 from mayo.session.train import Train
 from mayo.session.test import Test
 from mayo.session.eval import Evaluate
-from mayo.session.retrain import LayerwiseRetrain, GlobalRetrain
+from mayo.session.retrain.layer_retrain import LayerwiseRetrain
+from mayo.session.retrain.global_retrain import GlobalRetrain
 from mayo.session.profile import Profile
 
 
