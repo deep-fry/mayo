0a4cb3305003e3accc1e6264993feb7c92b61a6d
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2018-06-16 20:17:14 +0200

    avoid empty layers

--- mayo/net/tf/layers.py
+++ mayo/net/tf/layers.py
@@ -86,7 +86,10 @@ class Layers(TFNetBase):
     def _estimate_memory_bitops(self, node, info):
         # FIXME use overrider.estimate
         name = '{}/weights'.format(node.formatted_name())
-        o = self.overriders[node].get(name)
+        o = self.overriders.pop(node, None)
+        if o is None:
+            return info
+        o = o.get(name)
         if not o:
             return info
         num_elements = o.after.shape.num_elements()
