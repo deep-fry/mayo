Unable to fix doubly applied gradient overrider for weighs with loss

mayo/net/tf/transform.py
@@ -1,4 +1,5 @@
 import copy
+import functools
 import contextlib
 
 import tensorflow as tf
@@ -6,7 +7,8 @@ from tensorflow.contrib import slim
 
 from mayo.log import log
 from mayo.util import (
-    import_from_string, object_from_params, multi_objects_from_params)
+    import_from_string, object_from_params, multi_objects_from_params,
+    compose_functions)
 from mayo.override import ChainOverrider
 
 
@@ -74,18 +76,21 @@ class ParameterTransformer(object):
                 continue
             overrider_params[key] = create_overrider(p)
 
-    @staticmethod
-    def custom_gradient(node, name, overrider):
-        def wrapped(op, grad):
-            log.debug(
-                'Overriding the gradient of {!r} from {!r} with {!r}.'
-                .format(name, grad, overrider))
-            # when overriding gradient, we are not inside any variable
-            # scope, so we use the full name for overrider hyperparameter
-            # instantiation
-            scope = '{}/gradient'.format(name)
-            return overrider.apply(node, scope, tf.get_variable, grad)
-        return wrapped
+    def _apply_gradient_overrider(self, node, name, overrider, tensor):
+        @tf.custom_gradient
+        def wrapped(v):
+            def gradient(grad):
+                log.debug(
+                    'Overriding the gradient of {!r} from {!r} with {!r}.'
+                    .format(name, grad, overrider))
+                # when overriding gradient, we are not inside any variable
+                # scope, so we use the full name for overrider hyperparameter
+                # instantiation
+                with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+                    return overrider.apply(
+                        node, 'gradient', tf.get_variable, grad)
+            return v, gradient
+        return wrapped(tensor)
 
     def _config_layer(self, node, params):
         # normalizer_fn and activation_fn
@@ -100,41 +105,33 @@ class ParameterTransformer(object):
         if params.get('normalizer_fn', None):
             norm_params = params.setdefault('normalizer_params', {})
             norm_params['is_training'] = self.is_training
+        activation_functions = []
+        # gradient of error
+        gradient_overrider = params.get('overrider.gradient.error')
+        if gradient_overrider and self.is_training:
+            self._add_overrider(node, 'gradient.error', gradient_overrider)
+            def gradient_fn(v):
+                name = '{}/errors'.format(node.formatted_name())
+                return self._apply_gradient_overrider(
+                    node, name, gradient_overrider, v)
+            activation_functions.append(gradient_fn)
         # activation
         activation_overrider = params.get('overrider.activation', None)
         if activation_overrider:
+            self._add_overrider(node, 'activation', activation_overrider)
             override_fn = lambda x: activation_overrider.apply(
                 node, 'activations', tf.get_variable, x)
-            self._add_overrider(node, 'activation', activation_overrider)
-        else:
-            override_fn = None
-        # produce a default ReLU activation for overrider specifically
-        default_fn = tf.nn.relu if override_fn else None
-        # gradient of error
-        gradient_overrider = params.get('overrider.gradient.error')
-        if gradient_overrider:
-            self._add_overrider(node, 'gradient.error', gradient_overrider)
-            gradient_name = '{}/activations/gradient'.format(
-                node.formatted_name())
-            gradient_func = self.custom_gradient(
-                node, gradient_name, gradient_overrider)
-            tf.RegisterGradient(gradient_name)(gradient_func)
-            gradient_map = {'Identity': gradient_name}
-
-            def gradient_fn(v):
-                with self.session.tf_graph.gradient_override_map(gradient_map):
-                    return tf.identity(v)
-        else:
-            gradient_fn = None
+            activation_functions.append(override_fn)
+        # produce a default ReLU activation when overriders are used
+        default_fn = tf.nn.relu if activation_functions else None
         activation_fn = params.get('activation_fn', default_fn)
-        activation_params = params.pop('activation_params', {})
-        if override_fn or activation_fn or gradient_fn:
-            identity_fn = lambda x: x
-            gradient_fn = gradient_fn or identity_fn
-            override_fn = override_fn or identity_fn
-            activation_fn = activation_fn or identity_fn
-            params['activation_fn'] = lambda x: \
-                activation_fn(override_fn(gradient_fn(x)), **activation_params)
+        if activation_fn:
+            activation_params = params.pop('activation_params', {})
+            activation_fn = functools.partial(
+                activation_fn, **activation_params)
+            activation_functions.append(activation_fn)
+        if activation_functions:
+            params['activation_fn'] = compose_functions(activation_functions)
         # set up other parameters
         params['scope'] = node.name
         try:
@@ -163,12 +160,7 @@ class ParameterTransformer(object):
             # gradient overrider
             overrider = gradient_overriders.get(key)
             if overrider and self.is_training:
-                gradient_name = '{}/gradient'.format(name)
-                gradient_func = self.custom_gradient(node, name, overrider)
-                tf.RegisterGradient(gradient_name)(gradient_func)
-                gradient_map = {'Identity': gradient_name}
-                with self.session.tf_graph.gradient_override_map(gradient_map):
-                    v = tf.identity(v)
+                v = self._apply_gradient_overrider(node, name, overrider, v)
                 self._add_overrider(
                     node, 'gradient.{}'.format(name), overrider)
             self.variables.setdefault(node, {})[name] = v