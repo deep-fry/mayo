f6d0cd5264242348c05f170d2b9dfcbc6a2082d3
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2018-09-22 22:29:31 -0700

    add eval

--- scripts/gating/normalizer_search.py
+++ scripts/gating/normalizer_search.py
@@ -7,6 +7,7 @@ mayo_dir = "../../"
 target_key = 'l1'
 gpus = [0, 1]
 cmd_formatter = './my datasets/cifar10.yaml {} trainers/cifarnet.yaml system.checkpoint.load=gate100 train.learning_rate._initial=0.1 train.decay_steps=50 system.max_epochs=180 system.checkpoint.save.interval=10 system.num_gpus=2 system.visible_gpus=[{},{}] reset-num-epochs train'
+eval_cmd_formatter = './my datasets/cifar10.yaml {} trainers/cifarnet.yaml system.checkpoint.load=gate100 train.learning_rate._initial=0.1 train.decay_steps=50 system.max_epochs=180 system.checkpoint.save.interval=10 system.num_gpus=2 system.visible_gpus=[{},{}] eval-all'
 
 
 regularizer = {
@@ -33,4 +34,11 @@ for index, value in enumerate(values):
         gpus[0],
         gpus[1]
     )
+    eval_cmd = eval_cmd_formatter.format(
+        os.path.join('gate_configs', 'gate'+str(index)+'.yaml'),
+        gpus[0],
+        gpus[1]
+    )
     subprocess.call(cmd, cwd=mayo_dir, shell=True)
+    subprocess.call(eval_cmd, cwd=mayo_dir, shell=True)
+    subprocess.call("cp eval_all.csv eval_all{}.csv".format(index), cwd=mayo_dir, shell=True)
