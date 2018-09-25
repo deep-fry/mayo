02f9553359677dbdf336a0ed7b449e728dbe255d
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2018-09-26 07:47:15 +0800

    misc

--- scripts/fp_quan/run_fixedp_incre_quan.py
+++ scripts/fp_quan/run_fixedp_incre_quan.py
@@ -10,7 +10,7 @@ eval_cmd_formatter = './my datasets/imagenet.yaml models/override/{}.yaml models
 eval_cmd_formatter = './my datasets/imagenet.yaml models/override/{}.yaml models/override/quantize/fixed.yaml trainers/cifarnet.yaml system.checkpoint.load=pretrained train.learning_rate._initial=0.01 train.learning_rate.decay_steps=10 system.max_epochs=30 system.checkpoint.save.interval=1 system.num_gpus=2 system.visible_gpus=[{},{}] eval-all'
 
 # interval training
-for percentage in [0.25, 0.5, 0.75, 1.0]
+for percentage in [0.25, 0.5, 0.75, 1.0]:
     if percentage == 0.25:
         # model name, ckpt, max_epochs, gpu, gpu, interval, act interval
         cmd = cmd_formatter.format(model, 'test', 10, gpus[0], gpus[1], percentage, percentage)
