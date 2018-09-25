cbc7c6c2fae529f5d58e06a3442c9754fe81ab16
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2018-09-25 16:51:07 +0800

    add fp quan script

---
+++ scripts/fp_quan/run_fixedp_quan.py
@@ -0,0 +1,14 @@
+import yaml
+import os
+import subprocess
+
+mayo_dir = "../../"
+gpus = [0, 1]
+model='alexnet_bn'
+cmd_formatter = './my datasets/imagenet.yaml models/{}.yaml trainers/cifarnet.yaml system.checkpoint.load=pretrained train.learning_rate._initial=0.01 train.learning_rate.decay_steps=10 system.max_epochs=30 system.checkpoint.save.interval=1 system.num_gpus=2 system.visible_gpus=[{},{}] reset-num-epochs train'
+eval_cmd_formatter = './my datasets/imagenet.yaml models/{}.yaml trainers/cifarnet.yaml system.checkpoint.load=pretrained train.learning_rate._initial=0.01 train.learning_rate.decay_steps=10 system.max_epochs=30 system.checkpoint.save.interval=1 system.num_gpus=2 system.visible_gpus=[{},{}] eval-all'
+
+cmd = cmd_formatter.format(model, gpus[0], gpus[1])
+subprocess.call(cmd, cwd=mayo_dir, shell=True)
+subprocess.call(eval_cmd, cwd=mayo_dir, shell=True)
+subprocess.call("cp eval_all.csv eval_all{}.csv".format(model), cwd=mayo_dir, shell=True)
