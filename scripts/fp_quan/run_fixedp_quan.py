70fa470c51e3072890d0135c76cf4938ffaf7ede
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2018-09-25 17:06:50 +0800

    add quantizer file and cmd

--- scripts/fp_quan/run_fixedp_quan.py
+++ scripts/fp_quan/run_fixedp_quan.py
@@ -5,8 +5,8 @@ import subprocess
 mayo_dir = "../../"
 gpus = [0, 1]
 model='alexnet_bn'
-cmd_formatter = './my datasets/imagenet.yaml models/{}.yaml trainers/cifarnet.yaml system.checkpoint.load=pretrained train.learning_rate._initial=0.01 train.learning_rate.decay_steps=10 system.max_epochs=30 system.checkpoint.save.interval=1 system.num_gpus=2 system.visible_gpus=[{},{}] reset-num-epochs train'
-eval_cmd_formatter = './my datasets/imagenet.yaml models/{}.yaml trainers/cifarnet.yaml system.checkpoint.load=pretrained train.learning_rate._initial=0.01 train.learning_rate.decay_steps=10 system.max_epochs=30 system.checkpoint.save.interval=1 system.num_gpus=2 system.visible_gpus=[{},{}] eval-all'
+cmd_formatter = './my datasets/imagenet.yaml models/override/{}.yaml models/override/quantize/fixed.yaml trainers/cifarnet.yaml system.checkpoint.load=pretrained train.learning_rate._initial=0.01 train.learning_rate.decay_steps=10 system.max_epochs=30 system.checkpoint.save.interval=1 system.num_gpus=2 system.visible_gpus=[{},{}] reset-num-epochs train'
+eval_cmd_formatter = './my datasets/imagenet.yaml models/override/{}.yaml models/override/quantize/fixed.yaml trainers/cifarnet.yaml system.checkpoint.load=pretrained train.learning_rate._initial=0.01 train.learning_rate.decay_steps=10 system.max_epochs=30 system.checkpoint.save.interval=1 system.num_gpus=2 system.visible_gpus=[{},{}] eval-all'
 
 cmd = cmd_formatter.format(model, gpus[0], gpus[1])
 subprocess.call(cmd, cwd=mayo_dir, shell=True)
